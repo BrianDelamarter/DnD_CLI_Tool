4bde8406ec06dfbedc99d9b666a20b4fbb07fe1b
BrianDelamarter <brian.d.delamarter@gmail.com>
2026-08-28 15:48:37 -0700

    New project to create a DnD CLI Tool. Contains set up, beginner README, and some code

---
+++ README.md
@@ -0,0 +1,10 @@
+Welcome to the DnD CLI Tool
+
+This tool is designed to be a helpful addition to anyone playing DnD remotely. It is not designed
+to run campaign or even fully track characters or other aspects of the game. The primary focus will
+be a low distraction way to quickly get information and perform some quick actions rather than be
+an all encompassing tool.
+
+While it will hopefully prove helpful its primary function is to provide an opportunity to practice
+and learn new programming skills.
+
---
+++ main.py
@@ -0,0 +1,42 @@
+from print_effects import burn, printer, thunderstorm, wipe, figlet_format
+
+
+
+def main():
+    welcome()
+    
+
+def welcome():
+    burn(figlet_format("DnD CLI Tool!", "starwars"))
+    printer('''
+    Welcome to the DnD CLI Tool! This tool is designed to help you manage you play Dungeons and Dragons. 
+    You can use the command line to look up information about characters, monsters, spells, and more. 
+    Enjoy your adventure!''')
+    main_menu()
+
+def main_menu():
+    printer('''
+                              Main Menu:
+    1. Search Monsters    2. Search Spells    3. Roll Dice    4. Exit
+    ''')
+    choice = input("Please select an option (1-4): ")
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        printer("Exiting the DnD CLI Tool. Goodbye!")
+        exit()
+    else:
+        printer("Invalid choice. Please try again.")
+        main_menu()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
